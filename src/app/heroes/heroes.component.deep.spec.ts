3054ef7099bbc30427b1cea635ea77dc7276526d
khizerrehan <khizerrehan92@gmail.com>
2023-04-13 00:05:04 +0500

    Add test cases to test event emitter and eventHandler

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -6,6 +6,7 @@ import {HeroService} from "../hero.service";
 import {Hero} from "../hero";
 import {RouterTestingModule} from "@angular/router/testing";
 import {By} from "@angular/platform-browser";
+import {DebugElement} from "@angular/core";
 
 
 describe('HeroesComponent (Deep) Spec', () => {
@@ -47,9 +48,94 @@ describe('HeroesComponent (Deep) Spec', () => {
     expect(heroComponentsDEs.length).toEqual(3);
 
     for (let idx = 0; idx < heroComponentsDEs.length; idx++) {
-      console.log("Idx =>",idx, "=> Name:", HEROES[idx].name)
+      console.log("Idx =>", idx, "=> Name:", HEROES[idx].name)
       expect(heroComponentsDEs[idx].componentInstance.hero.name).toEqual(HEROES[idx].name)
     }
   });
+
+  // Approach-1
+  // This is something to trigger event by
+  // - grabbing DOM node
+  // - trigger certain event
+  // - do assertions
+
+  it('should call heroservice.delete when the hero component delete button using DOM element', () => {
+
+    spyOn(fixture.componentInstance, 'delete')
+
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    // run ngOnInit => which is fired by change detection
+    fixture.detectChanges()
+
+    /*
+     Info: A component is basically a subclass of a directive.It's more sort of specialized directive.
+    */
+
+    const heroComponentsDEs = fixture.debugElement.queryAll(By.directive(HeroComponent))
+    expect(heroComponentsDEs.length).toEqual(3);
+
+    heroComponentsDEs[0].query(By.css('button'))
+      .triggerEventHandler('click', {
+        stopPropagation: () => {
+        }
+      }) // pass dummy implementation for event.stopPropagation
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalled();
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+
+  })
+
+  // Approach-2
+  // This is something to trigger event by emitting even from EventEmitters instead of from DOM elements
+  // - grabbing component instance
+  // - access component event emitter property
+  // - emit event and validate against assertions.
+
+
+  it('should call heroservice.delete by emitting event emitter', () => {
+
+    spyOn(fixture.componentInstance, 'delete')
+
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    // run ngOnInit => which is fired by change detection
+    fixture.detectChanges()
+
+    /*
+     Info: A component is basically a subclass of a directive.It's more sort of specialized directive.
+    */
+
+    const heroComponentsDEs = fixture.debugElement.queryAll(By.directive(HeroComponent))
+    expect(heroComponentsDEs.length).toEqual(3);
+
+    // Fire EventEmitter from child to trigger event.
+    (<HeroComponent>heroComponentsDEs[1].componentInstance).delete.emit();
+    expect(fixture.componentInstance.delete).toHaveBeenCalled();
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[1]);
+
+  })
+
+  it('should call heroservice.delete by triggerEventHandler', () => {
+
+    spyOn(fixture.componentInstance, 'delete')
+
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    // run ngOnInit => which is fired by change detection
+    fixture.detectChanges()
+
+    /*
+     Info: A component is basically a subclass of a directive.It's more sort of specialized directive.
+    */
+
+    const heroComponentsDEs = fixture.debugElement.queryAll(By.directive(HeroComponent))
+    expect(heroComponentsDEs.length).toEqual(3);
+
+    // Fire Event which parent is listening (delete) using @Output is catched
+    // instead of making explicit trigger event from child and catch in parent.
+    // (delete)="delete(hero)" => its basically calling (delete) directly from here
+    (<DebugElement>heroComponentsDEs[2]).triggerEventHandler('delete', null)
+    expect(fixture.componentInstance.delete).toHaveBeenCalled();
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[2]);
+
+  })
 
 });
