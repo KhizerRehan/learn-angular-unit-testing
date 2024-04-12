1be86da78a0d0c68a9ecf3fbd577b350693e8482
Khizer Rehan <khizerrehan92@gmail.com>
2024-04-13 04:28:39 +0500

    Testing: Service

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,23 @@
+import { MessageService } from "./message.service";
+
+describe("service", () => {
+  let service: MessageService;
+  beforeEach(() => {
+    service = new MessageService();
+  });
+
+  it("should have no messages at start", () => {
+    expect(service.messages.length).toBe(0);
+  });
+
+  it("should have add a message when add is called", () => {
+    service.add("mewMessage");
+    expect(service.messages.length).toBe(1);
+  });
+
+  it("should clear messages when clear is called", () => {
+    service.add("mewMessage");
+    service.clear();
+    expect(service.messages.length).toBe(0);
+  });
+});
