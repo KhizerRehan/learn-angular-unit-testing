56f8eac6fdefc06ae962dd15acbff84aa94316f1
Khizer Rehan <khizerrehan92@gmail.com>
2024-05-06 13:05:32 +0500

    Add test for event handler and adding new hero

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -69,6 +69,72 @@ describe('HeroesComponent (Deep tests)', () => {
 
   })
 
+  it('should call heroService.deleteHero when the Hero Component\'s delete button is clicked', () => {
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+    expect(heroComponents.length).toBe(3);
+
+    heroComponents[0].query(By.css('button'))
+      .triggerEventHandler('click', { stopPropagation: () => { }});
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalledTimes(1);
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  })
+
+  it('should call heroService.deleteHero directly when the Hero Components delete button is clicked', () => {
+
+    /*
+     This test is same as above but it is more direct way to test the delete
+     event handler ia called direclty instead of using the button click event
+     as mimicking like child should be clicked and parent should be notified 
+     and catch the event.
+     so we are just check that delete event is called directly and parent 
+     doea catch the event.
+
+     shortcut way to test the delete event handler is called directly.
+
+    */
+
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+    heroComponents[0].triggerEventHandler('delete', HEROES[0]);
+
+    expect(fixture.componentInstance.delete).toHaveBeenCalledTimes(1);
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  })
+
+  it('should add a new hero to the hero list when the add button is clicked', () => {
+  
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const name = 'Mr. Khizer Rehan';
+    mockHeroService.addHero.and.returnValue(of({ id: 5, name, strength: 10 } as Hero));
+
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    inputElement.value = name;
+
+    const addButton = fixture.debugElement.queryAll(By.css('button'))[0];
+    addButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
+    expect(heroText).toContain(name);
+
+    // By Khizeer Matching exact HeroComponent
+    const heroComponentText = fixture.debugElement.query(By.css('ul > li:last-child')).nativeElement.textContent;
+    expect(heroComponentText).toContain(name);
+
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    expect(heroComponents.length).toBe(4);
+   
+  })
 
 
 })
